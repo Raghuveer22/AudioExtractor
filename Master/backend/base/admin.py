5e4c90575ba39dc6ef44f535aa05a522e921e9d9
Raghuveer22 <posa.mokshith@iitg.ac.in>
2023-05-20 01:29:01 +0530

    likes_comments

--- Master/backend/base/admin.py
+++ Master/backend/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Audio,Comment,Like
 # Register your models here.
+admin.site.register(Audio)
+admin.site.register(Comment)  
+admin.site.register(Like)
--- Master/backend/base/models.py
+++ Master/backend/base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
+import uuid
 # Create your models here.
+def upload_path(instance,filename):
+    return f'{instance.uploaded_by}/{filename}'
+
+class Audio(models.Model):
+    uploaded_by=models.ForeignKey(User, related_name='uploaded_by', on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
+    name=models.CharField(default="helo",max_length=100)
+    duration=models.DurationField(default=None)
+    upload_file = models.FileField(upload_to=upload_path, blank=True)
+    uploaded_on=models.DateTimeField(default=timezone.now())
+    
+class Comment(models.Model):
+    id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
+    text=models.CharField(max_length=256)
+    audio=models.ForeignKey(Audio,related_name='audio',on_delete=models.CASCADE)
+    timestamp=models.DurationField()
+    added_by=models.ForeignKey(User, related_name='added_by', on_delete=models.CASCADE)
+    added_on=models.DateTimeField(default=timezone.now())
+
+class Like(models.Model):
+    id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
+    comment=models.ForeignKey(Comment,related_name='comment',on_delete=models.CASCADE)
+    liked_by=models.ForeignKey(User, related_name='liked_by', on_delete=models.CASCADE)
+    liked_on=models.DateTimeField(default=timezone.now())
